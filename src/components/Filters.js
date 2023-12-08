22c9155435b28afd33b4c969185c2c554f73b3f2
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-08 22:56:29 +0300

    Completed the assignment

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,20 +1,29 @@
 import React from "react";
 
-function Filters() {
+function Filters({ onChangeType, onFindPetsClick }) {
+  function handleTypeChange(event) {
+    onChangeType(event.target.value);
+  }
+
+  function handleClick() {
+    onFindPetsClick();
+  }
+
   return (
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type" aria-label="type">
+        <select name="type" id="type" aria-label="type" onChange={handleTypeChange}>
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
           <option value="micropig">Micropigs</option>
         </select>
       </div>
-
       <div className="field">
-        <button className="ui secondary button">Find pets</button>
+        <button className="ui secondary button" onClick={handleClick}>
+          Find pets
+        </button>
       </div>
     </div>
   );
